55ca4a93731649fe9dbe907fcd9c7eae0092a1d6
Shijun Qin <272700767@qq.com>
2015-12-04 19:11:41 +0800

    添加GET comments :id的测试

---
+++ src/test/controller/restful/comments.test.js
@@ -0,0 +1,100 @@
+'use strict';
+
+const server = require('../../../server');
+const http = require('http');
+const request = require('supertest');
+const mm = require('mm');
+const should = require('should');
+
+describe('src/test/controllers/restful/albums.test.js', function() {
+  before(server.listen.bind(server, 0));
+  after(server.close.bind(server));
+  afterEach(mm.restore);
+
+  describe('GET /comments/:id', function() {
+    it('should get comment info OK', function(done) {
+      request(server)
+        .get('/comments/1')
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            return done(err);
+          }
+
+          res.body.statusCode.should.equal(200);
+          res.body.data.id.should.be.equal(1);
+          done();
+        });
+    });
+
+    it('should get comment 404', function(done) {
+      request(server)
+        .get('/comments/10000')
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            return done(err);
+          }
+
+          res.body.statusCode.should.equal(404);
+          res.body.message.should.be.equal('评论不存在');
+          done();
+        });
+    });
+
+    it('should get comment info with user OK', function(done) {
+      request(server)
+        .get('/comments/2?isWithUser=true')
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            return done(err);
+          }
+
+          res.body.statusCode.should.equal(200);
+          res.body.data.OrignalCommentId.should.be.a.Number();
+          res.body.data.User.id.should.be.a.Number();
+          done();
+        });
+    });
+
+    it('should get comment info with original comment OK', function(done) {
+      request(server)
+        .get('/comments/2?isWithOrignalComment=true')
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            return done(err);
+          }
+
+          res.body.statusCode.should.equal(200);
+          res.body.data.OrignalCommentId.should.be.a.Number();
+          res.body.data.OrignalComment.id.should.be.a.Number();
+          done();
+        });
+    });
+
+    it('should get comment info with user and original comment OK', function(done) {
+      request(server)
+        .get('/comments/2?isWithOrignalComment=true&isWithUser=true')
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            return done(err);
+          }
+
+          res.body.statusCode.should.equal(200);
+          res.body.data.OrignalCommentId.should.be.a.Number();
+          res.body.data.OrignalComment.id.should.be.a.Number();
+          res.body.data.OrignalComment.User.id.should.be.a.Number();
+          done();
+        });
+    });
+  });
+
+});
