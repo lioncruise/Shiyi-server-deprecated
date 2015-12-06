e268231e063c00eaccf53a419e4e7bde6288daef
Shijun Qin <272700767@qq.com>
2015-12-06 16:43:14 +0800

    添加restful message接口的测试

---
+++ src/test/controller/restful/messages.test.js
@@ -0,0 +1,136 @@
+'use strict';
+
+const server = require('../../../server');
+const http = require('http');
+const request = require('supertest');
+const mm = require('mm');
+const should = require('should');
+
+describe('src/test/controllers/restful/messages.test.js', function() {
+  before(server.listen.bind(server, 0));
+  after(server.close.bind(server));
+  afterEach(mm.restore);
+
+  describe('GET /messages/:id', function() {
+    it('should get message info OK', function(done) {
+      request(server)
+        .get('/messages/1')
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            return done(err);
+          }
+
+          res.body.statusCode.should.be.equal(200);
+          res.body.data.id.should.be.equal(1);
+          done();
+        });
+    });
+
+    it('should get message info 404', function(done) {
+      request(server)
+        .get('/messages/10000')
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            return done(err);
+          }
+
+          res.body.statusCode.should.be.equal(404);
+          res.body.message.should.be.equal('消息不存在');
+          done();
+        });
+    });
+
+    it('should get message info with user info OK', function(done) {
+      request(server)
+        .get('/messages/1?isWithUsersInfo=true')
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            return done(err);
+          }
+
+          res.body.statusCode.should.be.equal(200);
+          res.body.data.id.should.be.equal(1);
+          res.body.data.User.id.should.be.a.Number();
+          res.body.data.TargetUser.id.should.be.a.Number();
+          done();
+        });
+    });
+  });
+
+  describe('POST /messages', function() {
+    it('should create new message OK', function(done) {
+      const message = {
+        content: '嗨，美女',
+        TargetUserId: '2',
+      };
+
+      request(server)
+        .post('/messages')
+        .send(message)
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            return done(err);
+          }
+
+          res.body.statusCode.should.be.equal(200);
+          res.body.data.type.should.be.equal('letter');
+          res.body.data.content.should.be.equal('嗨，美女');
+          res.body.data.UserId.should.be.equal(1);
+          res.body.data.TargetUserId.should.be.equal('2');
+          done();
+        });
+    });
+
+    it('should create new message 422', function(done) {
+      const message = {
+        content: '',
+        TargetUserId: '2',
+      };
+
+      request(server)
+        .post('/messages')
+        .send(message)
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            return done(err);
+          }
+
+          res.body.statusCode.should.be.equal(422);
+          res.body.message.should.be.equal('Validation Failed');
+          done();
+        });
+    });
+
+    it('should create new message 422', function(done) {
+      const message = {
+        content: 'hello',
+      };
+
+      request(server)
+        .post('/messages')
+        .send(message)
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            return done(err);
+          }
+
+          res.body.statusCode.should.be.equal(422);
+          res.body.message.should.be.equal('Validation Failed');
+          done();
+        });
+    });
+
+  });
+});
