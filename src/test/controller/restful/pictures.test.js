8b29d3dff0d96259a977ba77da03819637f6a2b7
Shijun Qin <272700767@qq.com>
2015-12-07 10:38:46 +0800

    添加POST picture接口测试

--- src/test/controller/restful/pictures.test.js
+++ src/test/controller/restful/pictures.test.js
@@ -14,7 +14,7 @@ describe('src/test/controllers/restful/pictures.test.js', function() {
   describe('GET /pictures/:id', function() {
     it('should get picture info OK', function(done) {
       request(server)
-        .get('/pictures/1')
+        .get('/pictures/8')
         .expect('Content-type', 'application/json; charset=utf-8')
         .expect(200)
         .end(function(err, res) {
@@ -23,7 +23,7 @@ describe('src/test/controllers/restful/pictures.test.js', function() {
           }
 
           res.body.statusCode.should.be.equal(200);
-          res.body.data.id.should.be.equal(1);
+          res.body.data.id.should.be.equal(8);
           res.body.data.UserId.should.be.a.Number();
           res.body.data.AlbumId.should.be.a.Number();
           res.body.data.MemoryId.should.be.a.Number();
@@ -119,5 +119,60 @@ describe('src/test/controllers/restful/pictures.test.js', function() {
           done();
         });
     });
+  });
+
+  describe('POST /pictures', function() {
+    it('should create picture info OK', function(done) {
+      const picture = {
+        storeKey: '123.jpg',
+        type: 'picture',
+        AlbumId: '2',
+        MemoryId: '5',
+      };
+      request(server)
+        .post('/pictures')
+        .send(picture)
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            return done(err);
+          }
+
+          res.body.statusCode.should.be.equal(200);
+          res.body.data.UserId.should.be.equal(1);
+          res.body.data.AlbumId.should.be.equal('2');
+          res.body.data.MemoryId.should.be.equal('5');
+          res.body.data.type.should.be.equal('picture');
+          done();
+        });
+    });
+
+    it('should create video info OK', function(done) {
+      const video = {
+        storeKey: '123.jpg',
+        type: 'video',
+        AlbumId: '2',
+        MemoryId: '5',
+      };
+      request(server)
+        .post('/pictures')
+        .send(video)
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            return done(err);
+          }
+
+          res.body.statusCode.should.be.equal(200);
+          res.body.data.UserId.should.be.equal(1);
+          res.body.data.AlbumId.should.be.equal('2');
+          res.body.data.MemoryId.should.be.equal('5');
+          res.body.data.type.should.be.equal('video');
+          done();
+        });
+    });
+
   });
 });
