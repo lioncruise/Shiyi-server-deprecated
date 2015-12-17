82dea5097f6ffe771c4de5a4c71c24a5d6911490
wxygeek <wxygeek.abc@gmail.com>
2015-12-17 22:40:24 +0800

    增加新建pictures时的验证

--- src/controller/restful/pictures.js
+++ src/controller/restful/pictures.js
@@ -95,6 +95,35 @@ exports.create = function*() {
     },
   });
 
+  const album = yield models.Album.find({
+    paranoid: true,
+    where: {
+      id: this.request.body.AlbumId,
+    },
+  });
+  if (!album) {
+    this.body = {
+      statusCode: 404,
+      message: '相册不存在',
+    };
+    return;
+  }
+
+  const memory = yield models.Memory.find({
+    paranoid: true,
+    where: {
+      id: this.request.body.MemoryId,
+    },
+  });
+  if (!memory) {
+    this.body = {
+      statusCode: 404,
+      message: '记忆不存在',
+    };
+    return;
+  }
+
+
   const picture = yield models.Picture.create(Object.assign(this.request.body, {
     UserId: this.session.user.id,
   }));
