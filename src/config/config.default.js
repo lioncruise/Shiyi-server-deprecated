0e6b5eceacc6ad90eea873dae2f7c5f20d9131ca
wxygeek <wxygeek.abc@gmail.com>
2015-12-28 21:40:08 +0800

    更改config.js名称

---
+++ src/config/config.default.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  debug: false,
+  db: {
+    host: '127.0.0.1',
+    database: 'shiyi',
+    dialect: 'mysql',
+    username: 'root',
+    password: 'admin',
+    pool: {
+      max: 100,
+      min: 0,
+      idle: 5000,
+      maxConnections: 100,
+      minConnections: 0,
+      maxIdleTime: 5000,
+    },
+    logging: false,
+  },
+};
