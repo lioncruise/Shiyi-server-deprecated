2e0a8b6116dc33d106946108437205291532f156
wxygeek <wxygeek.abc@gmail.com>
2016-01-13 23:47:55 +0800

    更新getAllUnreadMessagesWithOneUser接口的order

--- src/controller/messages.js
+++ src/controller/messages.js
@@ -61,7 +61,7 @@ router.get('/getAllUnreadMessagesWithOneUser', function*() {
       TargetUserId: this.session.user.id,
     },
     order: [
-      ['createdAt', 'DESC'],
+      ['createdAt'],
     ],
   });
 
