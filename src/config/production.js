574e604cd7cbb2544f345c5e043c24f72b55cc30
liu946 <mkliuyag@163.com>
2016-01-17 21:30:11 +0800

    getui production config

--- src/config/production.js
+++ src/config/production.js
@@ -18,4 +18,13 @@ module.exports = {
     port: 6379,
     password: 'f62bf74d09af44c2:ShiyiServer233',
   },
+  getui: {
+    host: 'https://api.getui.com/apiex.htm',
+    appId: 'PdTKItsviK8EmwYhSwCpg3',
+    appKey: '7VcV5RODTz5BEiqtpXPPf5',
+    appSecert:'GZz4wca1mj9qbLrTsYyld3',
+    masterSecret: '9kRM3KSp5a8LEQd7yIS1T6',
+    isOffLine: true,
+    offlineExpireTime: 3600 * 12 * 1000,
+  },
 };
