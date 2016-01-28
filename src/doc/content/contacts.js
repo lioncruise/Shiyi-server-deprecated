a3df3960724034770eb88fe3ff3170a2a76b24d3
wxygeek <wxygeek.abc@gmail.com>
2016-01-28 16:51:24 +0800

    完善doc文字备注

--- src/doc/content/contacts.js
+++ src/doc/content/contacts.js
@@ -4,7 +4,7 @@ module.exports = [{
   fileName: 'POST getUsersByPhones',
   func: '按手机号查找已注册用户，通讯录匹配',
   note: [
-          '1.    3:互相关注 2:对方关注我 1:我关注对方 0:互不关注 字符串类型',
+          '1.返回的每个user里面，包含一个relation字段，类型为字符串类型    3:互相关注 2:对方关注我 1:我关注对方 0:互不关注 ',
           '2.返回的每个user里面，包含一个name字段，为上传时的那个name',
           ],
   requests: [
