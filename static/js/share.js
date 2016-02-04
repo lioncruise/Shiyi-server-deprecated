63828d9d899567ca4b3b23af26754bb67b73c564
liu946 <mkliuyag@163.com>
2016-02-04 12:28:21 +0800

    调整文件结构

---
+++ static/js/share.js
@@ -0,0 +1,14 @@
+function getUrlParam(name) {
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+  var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+  if (r != null) return unescape(r[2]); return null; //返回参数值
+}
+$('#qr_container').qrcode({render:"canvas",height:200, width:200,correctLevel:0,text:
+window.location.protocol + '//' + window.location.host + window.location.pathname + '?id=' + getUrlParam('id')
+});
+$('#url').attr('href', window.location.protocol + '//' + window.location.host + window.location.pathname + '?id=' + getUrlParam('id'));
+
+// 居中显示图片
+$('.show-image').each(function () {
+  $(this).css('margin-left' , (- ($(this).width() - window.innerWidth / 2) / 2) + 'px');
+});
